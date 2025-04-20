8b2415c05ad0a90fed0009d7ba3c1a862d38c1ae
김덕배 <97142351+Kim-Dukbae@users.noreply.github.com>
2025-04-20 13:44:23 +0900

    Create dataset_splitter.py

---
+++ datasets/dataset_splitter.py
@@ -0,0 +1,14 @@
+train_names = [
+    "IR_Case_017.png", "IR_Case_019.png", "IR_Case_020.png", "IR_Case_022.png",
+    "IR_Case_023.png", "IR_Case_026.png", "IR_Case_034.png", "IR_Case_035.png",
+    "IR_Case_036.png", "IR_Case_040.png", "IR_Case_041.png", "IR_Case_043.png",
+    "IR_Case_044.png", "IR_Case_049.png", "IR_Case_051.png", "IR_Case_052.png"
+]
+
+val_names = [
+    "IR_Case_053.png", "IR_Case_055.png", "IR_Case_056.png"
+]
+
+test_names = [
+    "IR_Case_011.png", "IR_Case_021.png", "IR_Case_038.png", "IR_Case_058.png"
+]
